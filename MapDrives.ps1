6746cfcd3b3beff58aed34dd061e5c24409d324b
Omnialias <jwang611@gmail.com>
2019-09-04 08:08:00 -0400

    New Scripts

---
+++ MapDrives.ps1
@@ -0,0 +1,17 @@
+﻿$wshell = New-Object -ComObject Wscript.Shell
+$wshell.Popup("Please type in your network username and password",0,"Done",0x1)
+$Cred = Get-Credential
+$user = $cred.UserName
+while ( $user -like "*@donohoe.com" ) {
+$wshell2 = New-Object -ComObject Wscript.Shell
+$wshell2.Popup("Please enter only your username. Do not enter @donohoe.com.",0,"Done",0x1)
+$Cred = Get-Credential
+$user = $cred.UserName
+}
+$LDrive = "L:"
+$LDrivePath = "\\NAS6\DCC"
+$GDrive = "G:"
+if ( $user -like "HQ\*" ) { $GDrivePath = "\\NAS1\"+$user.Substring(3) } else { $GDrivePath = "\\NAS1\"+$user }
+$net = New-Object -com WScript.Network
+$net.mapnetworkdrive($Ldrive, $LDrivepath, "true", $user, $cred.GetNetworkCredential().Password)
+$net.mapnetworkdrive($Gdrive, $GDrivepath, "true", $user, $cred.GetNetworkCredential().Password)
---
+++ TEST_CBSDNSChange.ps1
@@ -0,0 +1,24 @@
+﻿# Test-Connection -Quiet returns $True if any ping succeeds and returns $False if all pings fail
+$pingASA = Get-content C:\temp\test1.txt
+$pingSwitch = Get-content C:\Temp\test2.txt
+If ( 
+    ( $pingASA -eq "Off" ) -and ( $pingSwitch -eq "Off" )
+) { 
+    $Date = Get-Date
+    "$Date" + " - It ain't working." | Out-file -FilePath C:\Temp\TestOut.txt -Append 
+} 
+While ( 
+    ( $pingASA -eq "Off" ) -and ( $pingSwitch -eq "Off" )
+ ) {
+    $Date = Get-Date
+    "$Date" + " - It still ain't working." | Out-file -FilePath C:\Temp\TestOut.txt -Append 
+    sleep 10
+    $pingASA = Get-content C:\temp\test1.txt
+    $pingSwitch = Get-content C:\Temp\test2.txt
+}
+If ( 
+    ( $pingASA -eq "On" ) -Or ( $pingSwitch -eq "On" )
+) { 
+    $Date = Get-Date
+    "$Date" + " - It started working." | Out-file -FilePath C:\Temp\TestOut.txt -Append 
+} 
