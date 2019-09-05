25c0a029eeb477458f77d73e6f840ec91071c4fb
Omnialias <jwang611@gmail.com>
2019-09-05 09:06:52 -0400

    Updates

---
+++ MapDrives/InstallMapDrives.bat
@@ -0,0 +1,4 @@
+mkdir C:\Scripts
+CD /d "%~dp0"
+cp "Map Share Drives.lnk" "C:\Users\Public\Desktop\Map Share Drives.lnk"
+cp "MapDrives.ps1" "C:\Scripts\MapDrives.ps1"
--- MapDrives/MapDrives.ps1
+++ MapDrives/MapDrives.ps1
@@ -1,17 +1,25 @@
-﻿$wshell = New-Object -ComObject Wscript.Shell
+﻿# Remove existing L and G drive mappings
+$ErrorActionPreference = "SilentlyContinue"
+net use L: /delete >nul 2>&1
+net use G: /delete >nul 2>&1
+
+# Prompt for credentials
+$wshell = New-Object -ComObject Wscript.Shell
 $wshell.Popup("Please type in your network username and password",0,"Done",0x1)
 $Cred = Get-Credential
-$user = $cred.UserName
-while ( $user -like "*@donohoe.com" ) {
-$wshell2 = New-Object -ComObject Wscript.Shell
-$wshell2.Popup("Please enter only your username. Do not enter @donohoe.com.",0,"Done",0x1)
-$Cred = Get-Credential
-$user = $cred.UserName
-}
+$rawuser = $cred.UserName
+
+# Check if user typed "SAM@donohoe.com." Replace with "HQ\SAM." Otherwise if user typed "SAM," replace with "HQ\SAM." Result should always be $user = "HQ\SAM"
+if ( $rawuser -like "*@donohoe.com" ) { $user = "HQ\"+$rawuser.Substring(0,$rawuser.Length-12) } elseif ( $rawuser -notlike "HQ\*" ) { $user = "HQ\"+$rawuser }
+
+
+# Define static variables
 $LDrive = "L:"
 $LDrivePath = "\\NAS6\DCC"
 $GDrive = "G:"
-if ( $user -like "HQ\*" ) { $GDrivePath = "\\NAS1\"+$user.Substring(3) } else { $GDrivePath = "\\NAS1\"+$user }
+$GDrivePath = "\\NAS1\"+$user.Substring(3)
+
+# Map the drives
 $net = New-Object -com WScript.Network
-$net.mapnetworkdrive($Ldrive, $LDrivepath, "true", $user, $cred.GetNetworkCredential().Password)
-$net.mapnetworkdrive($Gdrive, $GDrivepath, "true", $user, $cred.GetNetworkCredential().Password)
+$net.mapnetworkdrive($Gdrive, $GDrivepath, $true, $user, $cred.GetNetworkCredential().Password)
+$net.mapnetworkdrive($Ldrive, $LDrivepath, $true, $user, $cred.GetNetworkCredential().Password)
