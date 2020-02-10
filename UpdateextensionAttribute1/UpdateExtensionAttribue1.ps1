748d1171b04b22e095ea50e8d5117d63453c5516
JosephWAdmin <JosephWAdmin@HQ.Donohoe>
2020-02-10 10:49:28 -0500

    Added ProxyAddresses.

--- UpdateextensionAttribute1/UpdateExtensionAttribue1.ps1
+++ UpdateextensionAttribute1/UpdateExtensionAttribue1.ps1
@@ -1,5 +1,5 @@
-﻿$Users = Get-Aduser -Searchbase "OU=DRES,DC=HQ,DC=Donohoe" -Filter * -Properties SamAccountName | select SAMAccountName
+﻿$Users = Get-Aduser -Searchbase "OU=DHS,DC=HQ,DC=Donohoe" -Filter * -Properties SamAccountName | select SAMAccountName
 foreach ($user in $users) { 
     $email = $user.SamAccountName+"@donohoe.com"
-    Set-ADUser -Identity $user.SamAccountName -Replace @{extensionAttribute1=$email}
+    Set-ADUser -Identity $user.SamAccountName -Replace @{extensionAttribute1=$email;proxyaddresses=$email}
 }
